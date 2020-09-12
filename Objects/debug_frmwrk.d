1c5edc96620599ed50576f6a7f6e7306d1819a80
ParkHyeonJin <qokkop@ajou.ac.kr>
2020-09-12 13:45:52 +0900

    Update debug_frmwrk.d

--- Objects/debug_frmwrk.d
+++ Objects/debug_frmwrk.d
@@ -1,12 +1,12 @@
 .\objects\debug_frmwrk.o: ..\Huins_NXP_LPC1768_Library\Drivers\source\debug_frmwrk.c
 .\objects\debug_frmwrk.o: ..\Huins_NXP_LPC1768_Library\Drivers\include\debug_frmwrk.h
 .\objects\debug_frmwrk.o: ..\Huins_NXP_LPC1768_Library\Drivers\include\lpc17xx_uart.h
-.\objects\debug_frmwrk.o: C:\Keil_v5\ARM\INC\NXP\LPC17xx\LPC17xx.h
+.\objects\debug_frmwrk.o: C:\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\2.6.0\Device\Include\LPC17xx.h
 .\objects\debug_frmwrk.o: C:\Keil_v5\ARM\CMSIS\Include\core_cm3.h
 .\objects\debug_frmwrk.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\debug_frmwrk.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmInstr.h
 .\objects\debug_frmwrk.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmFunc.h
-.\objects\debug_frmwrk.o: C:\Keil_v5\ARM\INC\NXP\LPC17xx\system_LPC17xx.h
+.\objects\debug_frmwrk.o: C:\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\2.6.0\Device\Include\system_LPC17xx.h
 .\objects\debug_frmwrk.o: ..\Huins_NXP_LPC1768_Library\Drivers\include\lpc_types.h
 .\objects\debug_frmwrk.o: ..\Huins_NXP_LPC1768_Library\Drivers\include\lpc17xx_gpio.h
 .\objects\debug_frmwrk.o: ..\Huins_NXP_LPC1768_Library\Drivers\include\lpc17xx_pinsel.h
