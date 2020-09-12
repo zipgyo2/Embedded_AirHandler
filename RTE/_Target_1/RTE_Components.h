8a71705a28d7a36aa6f38b922e177aed56cc7818
ParkHyeonJin <qokkop@ajou.ac.kr>
2020-09-12 13:49:22 +0900

    Update RTE_Components.h

--- RTE/_Target_1/RTE_Components.h
+++ RTE/_Target_1/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'main' 
+ * Project: 'AirHandler' 
  * Target:  'Target 1' 
  */
 
