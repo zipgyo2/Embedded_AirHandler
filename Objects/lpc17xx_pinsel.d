ea19011a5525bee24c3fe3ae4283aa4b7b1b0901
ParkHyeonJin <qokkop@ajou.ac.kr>
2020-09-12 13:48:03 +0900

    Update lpc17xx_pinsel.d

--- Objects/lpc17xx_pinsel.d
+++ Objects/lpc17xx_pinsel.d
@@ -1,9 +1,9 @@
 .\objects\lpc17xx_pinsel.o: ..\Huins_NXP_LPC1768_Library\Drivers\source\lpc17xx_pinsel.c
 .\objects\lpc17xx_pinsel.o: ..\Huins_NXP_LPC1768_Library\Drivers\include\lpc17xx_pinsel.h
-.\objects\lpc17xx_pinsel.o: C:\Keil_v5\ARM\INC\NXP\LPC17xx\LPC17xx.h
+.\objects\lpc17xx_pinsel.o: C:\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\2.6.0\Device\Include\LPC17xx.h
 .\objects\lpc17xx_pinsel.o: C:\Keil_v5\ARM\CMSIS\Include\core_cm3.h
 .\objects\lpc17xx_pinsel.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\lpc17xx_pinsel.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmInstr.h
 .\objects\lpc17xx_pinsel.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmFunc.h
-.\objects\lpc17xx_pinsel.o: C:\Keil_v5\ARM\INC\NXP\LPC17xx\system_LPC17xx.h
+.\objects\lpc17xx_pinsel.o: C:\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\2.6.0\Device\Include\system_LPC17xx.h
 .\objects\lpc17xx_pinsel.o: ..\Huins_NXP_LPC1768_Library\Drivers\include\lpc_types.h
