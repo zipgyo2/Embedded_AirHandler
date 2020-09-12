2ff03ebbf4ff9e8670697b266793564fe56a1738
ParkHyeonJin <qokkop@ajou.ac.kr>
2020-09-12 13:44:20 +0900

    Create motor.h

---
+++ Driver/header/motor.h
@@ -0,0 +1,16 @@
+/******************************************************************************
+  * @file  motor.h
+  * @author  집중교육 2조
+  * @version V1.0.0
+  * @date    2020. 09. 11
+  * @brief   MOTOR Driver	Header File
+  ******************************************************************************/
+
+#ifndef _MOTOR_H_
+#define _MOTOR_H_
+#include "LPC17XX.h"
+	
+void Motor_Init (void);
+void Cycle(uint32_t adc_value);
+
+#endif
